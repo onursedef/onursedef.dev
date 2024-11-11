b2dea33cb6f7e52e252e7f2e19a923d91e350fb4
onursedef <onrsedef@gmail.com>
2024-11-11 22:41:43 +0300

    feat(social-card): add social-card component

---
+++ src/lib/components/atomic/social-card.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	let {
+		size,
+		background,
+		link,
+		titleBg,
+		title,
+		titleColor,
+		description,
+		iconColor,
+		children,
+		border,
+		borderColor
+	} = $props<{
+		size: 'sm' | 'md' | 'lg';
+		background: string;
+		link: string;
+		titleBg: string;
+		title: string;
+		titleColor: string;
+		description: string;
+		iconColor: string;
+		children: any;
+		border: boolean;
+		borderColor: string;
+	}>();
+</script>
+
+<div
+	class="{background} relative h-24 w-full rounded-lg bg-left {border
+		? `border ${borderColor}`
+		: ''} overflow-hidden"
+>
+	<div class="flex h-full w-full items-center justify-between px-4">
+		<div class="flex flex-col items-center gap-1 text-left">
+			<a
+				href={link}
+				class="{titleBg} cursor-pointer select-none rounded-2xl px-3 py-1 text-sm transition-colors {titleColor}"
+				>{title}</a
+			>
+			<p class="select-none text-left text-sm">{description}</p>
+		</div>
+		<div class="absolute right-2 h-28 w-28 rounded-lg {iconColor}">
+			{@render children()}
+		</div>
+	</div>
+</div>
