ef5661ebe7b38dbab4573d691c0159eba8643f6f
onursedef <onrsedef@gmail.com>
2024-11-11 22:42:18 +0300

    feat(tooltip): move tooltips to respective component

---
+++ src/lib/components/atomic/tooltip-menu-button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { createTooltip, melt } from '@melt-ui/svelte';
+	import { Command } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
+
+	const {
+		elements: { trigger, content, arrow },
+		states: { open }
+	} = createTooltip({
+		positioning: {
+			placement: 'bottom'
+		},
+		openDelay: 0,
+		closeDelay: 0,
+		closeOnPointerDown: false,
+		forceVisible: true
+	});
+</script>
+
+<button
+	use:melt={$trigger}
+	aria-roledescription="tooltip"
+	aria-label="Open Menu"
+	class="cursor-pointer rounded-lg border border-indigo-800/30 bg-indigo-600/30 p-3 transition-all hover:bg-indigo-600"
+>
+	<Command class="aspect-1 w-5" />
+</button>
+
+{#if $open}
+	<div
+		use:melt={$content}
+		transition:fade={{ duration: 100 }}
+		class=" z-10 rounded-lg bg-slate-950 shadow-lg"
+	>
+		<div use:melt={$arrow}></div>
+		<p class="px-2 py-1 text-sm text-white">Open Menu</p>
+	</div>
+{/if}
