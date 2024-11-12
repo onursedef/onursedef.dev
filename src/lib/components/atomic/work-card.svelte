dae1e6d4ed9f7e6e839c9698a35d1cddc0c8fde3
onursedef <onrsedef@gmail.com>
2024-11-12 21:21:12 +0300

    feat(work-card): add work card component with GitHub and external links

---
+++ src/lib/components/atomic/work-card.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { ExternalLink } from "lucide-svelte";
+	import Github from "./icons/github.svelte";
+
+    let {
+        image,
+        title,
+        category,
+        description,
+        githubLink,
+        webLink
+    } = $props<{
+        image: string;
+        title: string;
+        category: string;
+        description: string;
+        githubLink: string | null;
+        webLink: string | null;
+    }>();
+</script>
+
+<div class="flex flex-col gap-3 rounded-lg p-2 transition-colors hover:bg-slate-800/65">
+    <img src="{image}" alt="placeholder" class="w-full aspect-[3/2] rounded-lg object-cover">
+    <div class="flex flex-col gap-1">
+        <div class="inline-flex justify-between items-center">
+            <h3 class="text-lg font-bold">{title}</h3>
+            <div class="inline-flex gap-5 items-center mt-1">
+                <a href="{githubLink}" class="fill-white w-8 h-8 hover:fill-gray-300 transition-colors">
+                     <Github />
+                </a>
+                <a href="{webLink}">
+                    <ExternalLink class="w-8 h-8 text-white hover:text-gray-300 transition-colors" />
+                </a>
+            </div>
+        </div>
+        <p class="items-center gap-4 text-xs text-gray-500">{category}</p>
+        <p class="line-clamp-2 text-wrap text-sm">
+            {description}
+        </p>
+        
+    </div>
+</div>
---
+++ src/routes/works/+page.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import CategoryChip from "$lib/components/atomic/category-chip.svelte";
+	import Github from "$lib/components/atomic/icons/github.svelte";
+	import { ExternalLink, GithubIcon } from "lucide-svelte";
+    import Placeholder from '../../assets/images/placeholder-1.jpg';
+	import WorkCard from "$lib/components/atomic/work-card.svelte";
+
+    const categories = [
+        {
+            name: 'Web Development',
+            slug: 'web-development'
+        },
+        {
+            name: 'Mobile',
+            slug: 'mobile'
+        },
+        {
+            name: 'Desktop',
+            slug: 'desktop'
+        },
+        {
+            name: 'Game',
+            slug: 'game'
+        }
+    ]
+
+    const works = [
+        {
+            image: Placeholder,
+            title: 'WebApp',
+            category: 'Web',
+            description: 'WebApp is a web application that is built with React and TailwindCSS. It is a simple web application that is built for learning purposes.',
+            githubLink: 'https://github.com',
+            webLink: 'https://webapp.com'
+        },
+        {
+            image: Placeholder,
+            title: 'MobileApp',
+            category: 'Mobile',
+            description: 'MobileApp is a mobile application that is built with React Native and TailwindCSS. It is a simple mobile application that is built for learning purposes.',
+            githubLink: 'https://github.com',
+            webLink: 'https://mobileapp.com'
+        },
+        {
+            image: Placeholder,
+            title: 'DesktopApp',
+            category: 'Desktop',
+            description: 'DesktopApp is a desktop application that is built with Electron and TailwindCSS. It is a simple desktop application that is built for learning purposes.',
+            githubLink: 'https://github.com',
+            webLink: 'https://desktopapp.com'
+        },
+        {
+            image: Placeholder,
+            title: 'GameApp',
+            category: 'Game',
+            description: 'GameApp is a game application that is built with Unity and TailwindCSS. It is a simple game application that is built for learning purposes.',
+            githubLink: 'https://github.com',
+            webLink: 'https://gameapp.com'
+        }
+    ]
+</script>
+
+<div class="flex flex-col gap-5">
+    <h1 class="text-3xl font-bold">Works</h1>
+    <div class="inline-flex gap-2">
+        <a
+			href=""
+			class="rounded-lg border-2 border-slate-800 bg-slate-700/65 px-6 py-1 transition-colors hover:bg-slate-800"
+		>
+			All
+		</a>
+        {#each categories as category}
+            <CategoryChip {...category} selected={false} />
+        {/each}
+    </div>
+    <div class="grid grid-cols-3 gap-2">
+        {#each works as work}
+            <WorkCard {...work} />
+        {/each}
+    </div>
+</div>
