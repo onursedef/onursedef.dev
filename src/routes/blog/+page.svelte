fa2c5cb2d23049d125e21400393ca19ea5885e5e
onursedef <onrsedef@gmail.com>
2024-11-13 15:04:01 +0300

    refactor(unauthenticated): move files

--- src/routes/blog/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import FeaturedPosts from '$lib/components/featured-posts.svelte';
-	import LatestPosts from '$lib/components/latest-posts.svelte';
-</script>
-
-<div class="flex flex-1 flex-col gap-10 p-5">
-	<FeaturedPosts />
-	<LatestPosts />
-</div>
