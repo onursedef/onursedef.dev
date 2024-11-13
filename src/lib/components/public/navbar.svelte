15f71a65d6e7019bc362eb82324e0a1dff564f8a
onursedef <onrsedef@gmail.com>
2024-11-13 23:46:03 +0300

    feat(navbar): implement dynamic logo switching based on theme

--- src/lib/components/public/navbar.svelte
+++ src/lib/components/public/navbar.svelte
@@ -4,11 +4,20 @@
 	import TooltipMenuButton from '../atomic/public/tooltip-menu-button.svelte';
 	import TooltipThemeChanger from '../atomic/public/tooltip-theme-changer.svelte';
 	import { theme } from '$lib/stores/theme';
+	import { writable } from 'svelte/store';
+
+	let logoSrc = writable(logo);
+
+	$effect(() => {
+		theme.subscribe((value) => {
+			logoSrc.set(value === 'dark' ? logo : logoBlack);
+		});
+	})
 </script>
 
 <nav class="flex flex-row justify-between px-3 py-5">
 	<a href="/" class="inline-flex items-center gap-5">
-		<img src={$theme === 'dark' ? logo : logoBlack} alt="logo" class="h-12 w-12" />
+		<img src={$logoSrc} alt="logo" class="h-12 w-12" />
 	</a>
 	<div class="inline-flex gap-4">
 		<TooltipMenuButton />
