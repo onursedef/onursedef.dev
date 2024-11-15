d38d56aa1f8b2df894e3b358fdba47781fdbd140
onursedef <onrsedef@gmail.com>
2024-11-15 23:47:53 +0300

    feat(sidebar): enhance sidebar functionality with dynamic path handling and improved tab structure

--- src/lib/components/admin/sidebar.svelte
+++ src/lib/components/admin/sidebar.svelte
@@ -1,83 +1,124 @@
-<script>
-	import { ChevronLeft, Dock, File, Home, ScrollText } from "lucide-svelte";
-	import SidebarTab from "../atomic/admin/sidebar-tab.svelte";
-    import Logo from '../../../assets/images/logo.svg';
-	import { writable } from "svelte/store";
+<script lang="ts">
+	import { ChevronLeft, Dock, File, Home, ScrollText } from 'lucide-svelte';
+	import SidebarTab from '../atomic/admin/sidebar-tab.svelte';
+	import Logo from '../../../assets/images/logo.svg';
+	import { writable } from 'svelte/store';
 
-    let isSidebarOpen = writable(true);
+	let isSidebarOpen = writable(true);
 
-    const toggleSidebar = () =>{
-        isSidebarOpen.update(value => !value);
-    }
+	const toggleSidebar = () => {
+		isSidebarOpen.update((value) => !value);
+	};
 
-    const tabs = [
-        {
-            title: 'Dashboard',
-            active: true,
-            href: '/admin',
-            items: null,
-            icon: Home
-        },
-        {
-            title: 'Blog',
-            active: false,
-            href: '',
-            items: [
-                {
-                    title: 'Posts',
-                    active: false,
-                    href: '/admin/posts'
-                },
-                {
-                    title: 'Categories',
-                    active: false,
-                    href: '/admin/categories'
-                },
-                {
-                    title: 'Tags',
-                    active: false,
-                    href: '/admin/tags'
-                },
-                {
-                    title: 'Comments',
-                    active: false,
-                    href: '/admin/comments'
-                }
-            ],
-            icon: ScrollText
-        },
-        {
-            title: 'Works',
-            active: false,
-            href: '/admin',
-            items: null,
-            icon: Dock
-        },
-        {
-            title: 'Resume',
-            active: false,
-            href: '/admin',
-            items: null,
-            icon: File
-        }
-    ]
+	let path = writable('/admin');
+
+	const tabs = [
+		{
+			title: 'Dashboard',
+			active: false,
+			href: '/admin',
+			items: null,
+			icon: Home
+		},
+		{
+			title: 'Blog',
+			active: false,
+			href: '',
+			items: [
+				{
+					title: 'Posts',
+					active: false,
+					href: '/admin/posts'
+				},
+				{
+					title: 'Categories',
+					active: false,
+					href: '/admin/categories'
+				},
+				{
+					title: 'Tags',
+					active: false,
+					href: '/admin/tags'
+				},
+				{
+					title: 'Comments',
+					active: false,
+					href: '/admin/comments'
+				}
+			],
+			icon: ScrollText
+		},
+		{
+			title: 'Works',
+			active: false,
+			href: '/admin/works',
+			items: null,
+			icon: Dock
+		},
+		{
+			title: 'Resume',
+			active: false,
+			href: '/admin/resume',
+			items: null,
+			icon: File
+		}
+	];
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const unsubscribe = path.subscribe((value) => {
+			console.log(value);
+			tabs.forEach((tab) => {
+				if (tab.items !== null) {
+					tab.items.forEach((item) => {
+						item.active = item.href === value;
+					});
+				} else {
+					tab.active = tab.href === value;
+				}
+			});
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	});
 </script>
 
-<div class="relative flex flex-col gap-5 transition-all duration-300 {$isSidebarOpen ? 'w-64' : 'w-20'} bg-slate-800 h-screen py-6 flex-shrink-0 border-r-2 border-slate-600">
-    <div class="inline-flex items-center justify-center w-full gap-2">
-        <img src="{Logo}" alt="logo" class="w-14 aspect-1">
-        <h1 class="text-2xl font-bold ml-2 {$isSidebarOpen ? 'fade-in': 'fade-out'}">Panel</h1>
-    </div>
-    <div class="absolute -right-3 top-48">
-        <button onclick="{toggleSidebar}" class="p-1 rounded-full bg-slate-500 hover:bg-slate-600 transition-colors">
-            <ChevronLeft class="w-4 h-4 text-white transition-transform duration-300 transform {$isSidebarOpen ? 'rotate-0' : 'rotate-180'}" />
-        </button>
-    </div>
-    <div class="flex flex-col gap-2">
-        {#each tabs as tab}
-            <SidebarTab title={tab.title} active={tab.active} href={tab.href} items={tab.items} sidebarOpen={$isSidebarOpen}>
-                <svelte:component this={tab.icon} class="w-6 h-6" />
-            </SidebarTab>
-        {/each}
-    </div>
+<div
+	class="relative flex flex-col gap-5 transition-all duration-300 {$isSidebarOpen
+		? 'w-64'
+		: 'w-20'} h-screen flex-shrink-0 border-r-2 border-slate-600 bg-slate-800 py-6"
+>
+	<div class="inline-flex w-full items-center justify-center gap-2">
+		<img src={Logo} alt="logo" class="aspect-1 w-14" />
+		<h1 class="ml-2 text-2xl font-bold {$isSidebarOpen ? 'fade-in' : 'fade-out'}">Panel</h1>
+	</div>
+	<div class="absolute -right-3 top-48">
+		<button
+			onclick={toggleSidebar}
+			class="rounded-full bg-slate-500 p-1 transition-colors hover:bg-slate-600"
+		>
+			<ChevronLeft
+				class="h-4 w-4 transform text-white transition-transform duration-300 {$isSidebarOpen
+					? 'rotate-0'
+					: 'rotate-180'}"
+			/>
+		</button>
+	</div>
+	<div class="flex flex-col gap-2">
+		{#each tabs as tab}
+					<SidebarTab
+					title={tab.title}
+					active={tab.active}
+					href={tab.href}
+					items={tab.items}
+					sidebarOpen={$isSidebarOpen}
+					path={path}
+				>
+					<svelte:component this={tab.icon} class="h-6 w-6" />
+				</SidebarTab>
+		{/each}
+	</div>
 </div>
--- src/lib/components/atomic/admin/sidebar-tab.svelte
+++ src/lib/components/atomic/admin/sidebar-tab.svelte
@@ -1,60 +1,107 @@
 <script lang="ts">
-	import { ChevronDown } from "lucide-svelte";
-    import { writable } from "svelte/store";
+	import { browser } from '$app/environment';
+	import { ChevronDown } from 'lucide-svelte';
+	import { writable, type Writable } from 'svelte/store';
 
-    let {
-        title,
-        active,
-        href,
-        sidebarOpen,
-        items,
-        children,
-    } = $props<{
-        title: string;
-        active: boolean,
-        href: string,
-        sidebarOpen: boolean,
-        items: {
-            title: string,
-            active: boolean,
-            href: string,
-        }[] | null,
-        children: any,
-    }>();
+	let { title, active, href, path, sidebarOpen, items, children } = $props<{
+		title: string;
+		active: boolean;
+		href: string;
+		path: Writable<string>;
+		sidebarOpen: boolean;
+		items:
+			| {
+					title: string;
+					active: boolean;
+					href: string;
+			  }[]
+			| null;
+		children: any;
+	}>();
 
-    var isDropdownOpen = writable(false);
+	var isDropdownOpen = writable(false);
 
-    const toggleDropdown = () => {
-        isDropdownOpen.update(value => !value);
-    }
+	const toggleDropdown = () => {
+		isDropdownOpen.update((value) => !value);
+	};
+
+	if (items) {
+		console.log(items.map((item: any) => item.active).includes(true));
+	}
+
+	$effect(() => {
+		if (items) {
+			isDropdownOpen.set(items.map((item: any) => item.active).includes(true));
+		}
+	});
+
+	const setOptions = (href: string) => {
+		path.set(href);
+		if (browser) {
+			window.location.href = href;
+		}
+		console.log('sidebar-tab', $path);
+	};
 </script>
 
 {#if items != null}
-    <button onclick={toggleDropdown} class="flex flex-col {$isDropdownOpen ? 'gap-2' : ''} items-start transition-colors py-3 hover:bg-slate-700 rounded-e-md {sidebarOpen ? 'w-56' : 'w-14'}  {active ? 'bg-slate-600' : ''}">
-        <div class="flex justify-between items-center px-6 w-full">
-            <div class="inline-flex items-center gap-2">
-                <div>{@render children()}</div>
-                <span class="font-semibold select-none {sidebarOpen ? 'fade-in' : 'fade-out'}">{title}</span>
-            </div>
-                <div class="{sidebarOpen ? 'translate-x-0' : '-translate-x-64'} transition-all ml-auto p-1 hover:bg-slate-300 text-slate-400 hover:text-slate-600 rounded-full text-left">
-                    <ChevronDown class="w-4 h-4 duration-300 transition-transform {$isDropdownOpen ? 'rotate-180' : 'rotate-0'}" />
-                </div>
-        </div>
-        <div class="flex flex-col gap-2 w-full transition-all duration-300 overflow-hidden items-start {$isDropdownOpen ? 'max-h-screen-2xl opacity-100' : 'h-0 max-h-0 opacity-0'}">
-            {#each items as item}
-                <a href="{item?.href}" class="pl-14 text-sm transition-colors hover:bg-slate-600 rounded-e-md px-4 py-2 w-full text-left {item.active ? 'bg-slate-600' : ''} w-full h-full cursor-pointer">
-                    <span>{item.title}</span>
-                </a>
-            {/each}
-        </div>
-    </button>
+	<button
+		onclick={toggleDropdown}
+		class="flex flex-col {$isDropdownOpen
+			? 'gap-2'
+			: ''} items-start rounded-e-md py-3 transition-colors hover:bg-slate-700 {sidebarOpen
+			? 'w-56'
+			: 'w-14'}  {items.map((item: any) => item.active).includes(true) ? 'bg-slate-600' : ''}"
+	>
+		<div class="flex w-full items-center justify-between px-6">
+			<div class="inline-flex items-center gap-2">
+				<div>{@render children()}</div>
+				<span class="select-none font-semibold {sidebarOpen ? 'fade-in' : 'fade-out'}">{title}</span
+				>
+			</div>
+			<div
+				class="{sidebarOpen
+					? 'translate-x-0'
+					: '-translate-x-64'} ml-auto rounded-full p-1 text-left text-slate-400 transition-all hover:bg-slate-300 hover:text-slate-600"
+			>
+				<ChevronDown
+					class="h-4 w-4 transition-transform duration-300 {$isDropdownOpen
+						? 'rotate-180'
+						: 'rotate-0'}"
+				/>
+			</div>
+		</div>
+		<div
+			class="flex w-full flex-col items-start gap-2 overflow-hidden transition-all duration-300 {$isDropdownOpen &&
+			sidebarOpen
+				? 'max-h-screen-2xl opacity-100'
+				: 'h-0 max-h-0 opacity-0'}"
+		>
+			{#each items as item}
+				<button
+					onclick={() => setOptions(item.href)}
+					class="w-full rounded-e-md px-4 py-2 pl-14 text-left text-sm transition-colors hover:bg-slate-600 {item.active
+						? 'bg-slate-500'
+						: ''} h-full w-full cursor-pointer"
+				>
+					<span>{item.title}</span>
+				</button>
+			{/each}
+		</div>
+	</button>
 {:else}
-    <a href="{href}" class="flex flex-col gap-2 items-start transition-colors py-3 hover:bg-slate-700 rounded-e-md {sidebarOpen ? 'w-56' : 'w-14'}  {active ? 'bg-slate-600' : ''}">
-        <div class="flex justify-between items-center px-6 w-full">
-            <div class="inline-flex items-center gap-2">
-                <div>{@render children()}</div>
-                <span class="font-semibold select-none {sidebarOpen ? 'fade-in' : 'fade-out'}">{title}</span>
-            </div>
-        </div>
-    </a>
+	<button
+		onclick="{() => setOptions(href)}"
+		class="flex flex-col items-start gap-2 rounded-e-md py-3 transition-colors hover:bg-slate-700 {sidebarOpen
+			? 'w-56'
+			: 'w-14'}  {active ? 'bg-slate-600' : ''}"
+	>
+		<div class="flex w-full items-center justify-between px-6">
+			<div class="inline-flex items-center gap-2">
+				<div>{@render children()}</div>
+				<span class="select-none font-semibold {sidebarOpen ? 'fade-in' : 'fade-out'}">{title}</span
+				>
+			</div>
+		</div>
+	</button>
 {/if}
