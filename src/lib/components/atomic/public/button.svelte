876d19c434dbb397bc8e18ebe2362d8225581796
onursedef <onrsedef@gmail.com>
2024-11-16 00:29:28 +0300

    feat(buttons): implement atomic button components with loading and disabled states

---
+++ src/lib/components/atomic/public/button.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import DangerButton from "./danger-button.svelte";
+	import InfoButton from "./info-button.svelte";
+	import PrimaryButton from "./primary-button.svelte";
+	import SecondaryButton from "./secondary-button.svelte";
+import SuccessButton from "./success-button.svelte";
+	import WarningButton from "./warning-button.svelte";
+
+    let {
+        onclick,
+        type,
+        disabled,
+        loading,
+        children,
+    } = $props<{
+        onclick: () => void;
+        type: 'success' | 'danger' | 'warning' | 'info' | 'primary' | 'secondary';
+        disabled: boolean;
+        loading: boolean;
+        children: any;
+    }>();
+</script>
+
+{#if type === 'success'}
+    <SuccessButton {onclick} {disabled} {loading}>{children}</SuccessButton>
+{:else if type === 'danger'}
+    <DangerButton {onclick} {disabled} {loading}>{children}</DangerButton>
+{:else if type === 'warning'}
+    <WarningButton {onclick} {disabled} {loading}>{children}</WarningButton>
+{:else if type === 'info'}
+    <InfoButton {onclick} {disabled} {loading}>{children}</InfoButton>
+{:else if type === 'secondary'}
+    <SecondaryButton {onclick} {disabled} {loading}>{children}</SecondaryButton>
+{:else}
+    <PrimaryButton {onclick} {disabled} {loading}>{children}</PrimaryButton>
+{/if}
---
+++ src/lib/components/atomic/public/warning-button.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Loader } from "lucide-svelte";
+
+    let {
+        onclick,
+        disabled,
+        loading,
+        children,
+    } = $props<{
+        onclick: () => void;
+        disabled: boolean;
+        loading: boolean;
+        children: any;
+    }>();
+</script>
+
+{#if disabled}
+<button disabled class="inline-flex items-center bg-yellow-600 px-4 py-2 rounded-md transition-colors hover:bg-yellow-800">
+    {@render children()}
+</button>
+{:else if loading}
+<button disabled class="inline-flex items-center bg-yellow-600 px-4 py-2 rounded-md transition-colors hover:bg-yellow-800">
+    <Loader class="h-5 w-5 text-white animate-spin" />
+    <div class="ml-2 inline-flex items-center">
+        {@render children()}
+    </div>
+</button>
+{:else}
+<button class="inline-flex items-center bg-yellow-600 px-4 py-2 rounded-md transition-colors hover:bg-yellow-800">
+    {@render children()}
+</button>
+{/if}
